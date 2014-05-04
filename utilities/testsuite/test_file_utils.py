e9bff7d15ebe8eb140dc06c44ee94e6c0e1bf839
gardenunez <gardenunez@gmail.com>
2014-05-04 13:46:40 +0200

    small modifications on file utils tests

--- utilities/testsuite/test_file_utils.py
+++ utilities/testsuite/test_file_utils.py
@@ -4,7 +4,6 @@
 Tests for file utils
 '''
 import unittest
-
 import utilities.miscutils.file_utils as file_utils
 
 
@@ -12,13 +11,19 @@ class TestFileUtils(unittest.TestCase):
     """
     File Utils test class
     """
+    def setUp(self):
+        """
+        Setup test class
+        """
+        self.valid_dirs = ['/bin', '/boot', '/home', '/tmp']
+        self.invalid_dir = '/unexistent'
 
     def test_check_dirs_true(self):
-        result = file_utils.check_dirs(['/bin', '/boot', '/home', '/tmp'])
+        result = file_utils.check_dirs(self.valid_dirs)
         self.assertTrue(result)
 
     def test_check_dirs_false(self):
-        result = file_utils.check_dirs(['/bin', '/unexistent', '/home'])
+        result = file_utils.check_dirs([self.valid_dirs[0], self.invalid_dir])
         self.assertFalse(result)
 
 if __name__ == "__main__":
